8c87662a9cfe46cf16092454597ad50d9703ba68
vladimir <vongrax@gmail.com>
2023-09-30 22:48:46 +0500

    feat: add webpack dev server

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,12 +1,19 @@
-export type BuildMode = "development" | "production"
+export type BuildMode = "development" | "production";
+
 export type BuildPaths = {
     entry: string;
     build: string;
-    html: string
+    html: string;
+}
+
+export type BuildEnv = {
+    mode: BuildMode;
+    port: number
 }
 
 export interface BuildOptions {
     mode: BuildMode;
-    paths: BuildPaths
-    isDev: boolean
+    paths: BuildPaths;
+    isDev: boolean;
+    port: number;
 }
