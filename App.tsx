6f405449e85ca81e815d4e545493cd107fd7cded
vladimir <vongrax@gmail.com>
2023-10-02 21:31:14 +0500

    feat: add router and create lazy

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from "./src/components/Counter";
+import React, {Suspense} from 'react';
 import "./index.scss"
+import {Link, Route, Routes} from "react-router-dom";
+import {MainPageAsync} from "./src/pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./src/pages/AboutPage/AboutPage.async";
 
 const App = () => {
     return (
         <div className="app">
-            this app
-           <Counter/>
+            <Link to={'/'}>Main page</Link>
+            <Link to={'/about'}>About page</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
